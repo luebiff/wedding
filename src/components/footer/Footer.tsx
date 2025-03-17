d987f1a5e3100e603717bf1dcabaeb18a731a017
luebiff <anders.wikrydberg@gmail.com>
2025-03-17 11:30:48 +0100

    footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const Navbar = () => {
+  const countdown = (): TimeLeft => {
+    const countDownDate = new Date("Jan 10, 2026 00:00:00").getTime();
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const timeLeft = {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(countdown());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(() => countdown());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex justify-center p-14 gap-4 border-t-1 border-gray-200 mt-8">
+      <div className="flex flex-col items-center">
+        <span className="text-5xl">{timeLeft.days}</span>
+        <p className="text-1xl">Dagar</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <span className="text-5xl">{timeLeft.hours}</span>
+        <p className="text-1xl">Timmar</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <span className="text-5xl">{timeLeft.minutes}</span>
+        <p className="text-1xl">Minuter</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <span className="text-5xl">{timeLeft.seconds}</span>
+        <p className="text-1xl">Sekunder</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
