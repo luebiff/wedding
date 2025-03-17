cfb06e83c3815fd2c3998a3ef06f6e37b1b4dd7f
luebiff <anders.wikrydberg@gmail.com>
2025-03-17 11:31:33 +0100

    hero and front page init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,43 @@
 import React from "react";
-import Header from "../../components/Header";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="relative isolate bg-gray-400 h-[calc(100vh-64px)]">
-        Home
+      <div className="flex items-center justify-center   bg-blend-overlay bg-gray-600 bg-[url(/src\assets\one_year.jpg)] bg-cover h-screen">
+        <div className="text-white flex flex-col items-center">
+          <div className="text-8xl">Anna & Anders</div>
+          <div className="text-6xl">2026-01-10</div>
+        </div>
       </div>
 
-      <div className="h-screen bg-gray-600">Content</div>
+      <div className="p-4 ">
+        <h2>Vi ska gifta oss! </h2>
+        <p>Äntligen bröllop!</p>
+        <p>
+          Det ska vi fira på Rånäs Slott! Du som fått vår inbjudan med länken
+          till denna hemsida är en av de personer som, på ett eller annat vis,
+          är de bästa i våra liv. Vi vill förstås att du ska komma till vårt
+          bröllop och dela den dagen med oss!
+        </p>
+        <p>
+          Vi har samlat all information här på hemsidan, på ett och samma ställe
+          för att underlätta för oss alla. Här finner du information om vigsel,
+          fest, boende och praktiska detaljer och lite annat som ni kan tänkas
+          vilja veta.
+        </p>
+        <p>
+          O.S.A. senast den 1 juli, speciellt viktigt för dig som vill bo på
+          Rånäs Slott över natten. Tveka inte att kontakta oss om ni undrar över
+          något.
+        </p>
+        <p>
+          Vi ser fram emot att se er på vårt bröllop! Kram, Sandra och Jonathan
+        </p>
+      </div>
+      <Footer />
     </>
   );
 };
