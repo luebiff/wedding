82751336419d7b53371a2d3b8d7e848dae6b5739
luebiff <anders.wikrydberg@gmail.com>
2025-04-23 13:00:49 +0200

    init info page

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -6,7 +6,71 @@ const Info = () => {
   return (
     <>
       <Header />
-      <div className="mt-[64px]">Info</div>
+      <div className="mt-[64px]">
+        <div>
+          <div>Övrig Information</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Viktiga personer</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Boende</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Tal & spex</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Klädkod</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Transport</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+        <div>
+          <div>Barn</div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            exercitationem ab aliquid nostrum, fugit quam quos, illo ad vitae
+            nesciunt libero excepturi. Illum optio neque facilis sit laudantium
+            quam quidem.
+          </p>
+        </div>
+      </div>
       <Footer />
     </>
   );
