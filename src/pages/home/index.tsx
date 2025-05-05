5cee6fb1cf48f89ea9134cac45636c61b588061b
luebiff <anders.wikrydberg@gmail.com>
2025-05-05 10:33:16 +0200

    contact page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,34 +12,35 @@ const Home = () => {
 
       <div className="grid grid-cols-2 gap-2 mb-10 p-10">
         <div>
-          <h2 className="text-3xl font-OleoScript">Vi ska gifta oss! </h2>
-          <p className="text-xl mb-3">Äntligen bröllop!</p>
-          <p>
-            Det ska vi fira på Rånäs Slott! Du som fått vår inbjudan med länken
-            till denna hemsida är en av de personer som, på ett eller annat vis,
-            är de bästa i våra liv. Vi vill förstås att du ska komma till vårt
-            bröllop och dela den dagen med oss!
-          </p>
-          <p>
-            Vi har samlat all information här på hemsidan, på ett och samma
-            ställe för att underlätta för oss alla. Här finner du information om
-            vigsel, fest, boende och praktiska detaljer och lite annat som ni
-            kan tänkas vilja veta.
-          </p>
-          <p>
-            O.S.A. senast den 1 juli, speciellt viktigt för dig som vill bo på
-            Rånäs Slott över natten. Tveka inte att kontakta oss om ni undrar
-            över något.
-          </p>
-          <p>
-            Vi ser fram emot att se er på vårt bröllop! Kram, Sandra och
-            Jonathan
-          </p>
+          <h2 className="text-3xl font-OleoScript mb-3">Vi ska gifta oss! </h2>
+          <div className="text-lg text-gray-700">
+            <p>
+              Det ska vi fira på Rånäs Slott! Du som fått vår inbjudan med
+              länken till denna hemsida är en av de personer som, på ett eller
+              annat vis, är de bästa i våra liv. Vi vill förstås att du ska
+              komma till vårt bröllop och dela den dagen med oss!
+            </p>
+            <p>
+              Vi har samlat all information här på hemsidan, på ett och samma
+              ställe för att underlätta för oss alla. Här finner du information
+              om vigsel, fest, boende och praktiska detaljer och lite annat som
+              ni kan tänkas vilja veta.
+            </p>
+            <p>
+              O.S.A. senast den 1 juli, speciellt viktigt för dig som vill bo på
+              Rånäs Slott över natten. Tveka inte att kontakta oss om ni undrar
+              över något.
+            </p>
+            <p>
+              Vi ser fram emot att se er på vårt bröllop! Kram, Sandra och
+              Jonathan
+            </p>
+          </div>
         </div>
         <img
           className="w-full h-96 object-cover mt-10"
           src="/src\assets\muskö.jpg"
-          alt=""
+          alt="Anna och Anders Muskö"
         />
       </div>
       <InfoCards />
